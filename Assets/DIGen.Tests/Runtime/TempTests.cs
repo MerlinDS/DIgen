3a79398872ca0ee8b6a53c31814528cfe144e5ac
MerlinDS <merlinkolv@gmail.com>
2025-03-18 21:07:58 +0200

    Add unity project and dependencies. Add a source generator project.

---
+++ Assets/DIGen.Tests/Runtime/TempTests.cs
@@ -0,0 +1,27 @@
+﻿using DIGen.Runtime;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DIGen.Tests.Runtime
+{
+    [TestFixture]
+    [TestOf(typeof(Temp))]
+    public class TempTests
+    {
+
+        [Test]
+        public void Test()
+        {
+            // Arrange
+            var temp = Substitute.For<Temp>();
+            temp.IsRunning.Returns(true);
+            
+            // Act
+            bool actual = temp.Run();
+            
+            // Assert
+            actual.Should().BeTrue();
+        }
+    }
+}
